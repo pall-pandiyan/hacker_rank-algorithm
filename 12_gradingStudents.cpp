5c6799232016dc8ad70d1df74e377edc176fb58f
Pall Pandiyan <pallpandiyan@gmail.com>
2022-05-07 12:56:44 +0530

    solved

---
+++ 12_gradingStudents.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*
+ * Complete the gradingStudents function below.
+ */
+vector<int> gradingStudents(vector<int> grades) {
+    int size = grades.size();
+    
+    for(int itr=0; itr<size; itr++) {
+        if (grades[itr] > 37) {
+            int bal = grades[itr] % 5;
+            bal = 5 - bal;
+            if (bal < 3) {
+                grades[itr] = grades[itr] + bal;
+            }
+        }
+    }
+    return grades;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    vector<int> grades(n);
+
+    for (int grades_itr = 0; grades_itr < n; grades_itr++) {
+        int grades_item;
+        cin >> grades_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        grades[grades_itr] = grades_item;
+    }
+
+    vector<int> result = gradingStudents(grades);
+
+    for (int result_itr = 0; result_itr < result.size(); result_itr++) {
+        fout << result[result_itr];
+
+        if (result_itr != result.size() - 1) {
+            fout << "\n";
+        }
+    }
+
+    fout << "\n";
+
+    fout.close();
+
+    return 0;
+}
